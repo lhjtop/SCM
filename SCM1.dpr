b01581abfaf9c35064cf822efb82cd66901f87a6
lhjtop <lhjtop@hanmail.net>
2014-02-24 20:39:17 +0900

    * 2014.2.24  드디어 GIT를 설치하다. 버전 관리를 해보자.  SCM(SosaCarManager)프로젝트를 델파이로 시작하기로 함.  델파이뿐만 아니라, 버전관리 툴인 GIT까지 배우기로 하고 실무 프로젝트에  바로 적용하여 VB로 작성한 프로젝트를 개발 종료하고, 업데이트 하기로 함.

---
+++ SCM1.dpr
@@ -0,0 +1,14 @@
+program SCM1;
+
+uses
+  Forms,
+  Main1 in 'Main1.pas' {MainFrm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TMainFrm, MainFrm);
+  Application.Run;
+end.
